8d7424d5702dc927151148991cac783d97cffe61
PaoloKahn <30302837+PaoloKahn@users.noreply.github.com>
2019-01-05 20:24:08 +0100

    command system

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const fs = require("fs");
 const config = require("./config.json");
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+client.config = config;
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.on('message', message => {
-    console.log(message.content);
-    if (message.content === '!ping') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('Pong.');
-    }
-});
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
+}
+
 
 client.login(config.token);
