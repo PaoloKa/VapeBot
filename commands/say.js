e90a60238e06914183842d8c6abe1257d16a9032
PaoloKahn <30302837+PaoloKahn@users.noreply.github.com>
2019-01-05 21:23:22 +0100

    tts added

--- commands/say.js
+++ commands/say.js
@@ -5,10 +5,10 @@ module.exports = {
     description: 'lets the bot speak for you',
     execute(message, args) {
         if (args.length < 1) {
-            throw 'Please provide a prefix to set!';
+            throw 'Please provide a message to say.';
         }
         message.delete();
         const words = args.join(" ")
-        message.channel.send(words);
+        message.channel.send("/tts "+words);
     },
 }
