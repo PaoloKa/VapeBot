377cd31777362da44534d9f1d1b9cb771ca06bcb
PaoloKahn <30302837+PaoloKahn@users.noreply.github.com>
2019-01-05 20:31:56 +0100

    command system

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ for (const file of commandFiles) {
     // with the key as the command name and the value as the exported module
     client.commands.set(command.name, command);
 }
+client.on('message', message => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    const args = message.content.slice(config.prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(message, args);
+    }
+    catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+});
+
+
 
 
 client.login(config.token);
