284ad18b1eb70653a6d7809776d82f741abfb5bc
PaoloKahn <30302837+PaoloKahn@users.noreply.github.com>
2019-05-29 13:50:50 +0200

    new dog api

--- bot/commands/dogo.js
+++ bot/commands/dogo.js
@@ -5,11 +5,11 @@ module.exports = {
     name: 'dogo',
     description: 'gives you a random picture of a dogo',
     execute(message, args) {
-        request(config.dog_API_url+"breeds/image/random", { json: true }, (err, res, body) => {
+        request("https://random.dog/woof.json", { json: true }, (err, res, body) => {
             if (err) { return console.log(err); }
-            console.log(body.message);
+            console.log(body.url);
             message.channel.send("Random dogo", {
-                file: body.message
+                files: [body.url]
             });
         });
     },
