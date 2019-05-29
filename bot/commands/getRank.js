99566aa57fd4285bc3a7d43178e8fdb020dbcf23
PaoloKahn <30302837+PaoloKahn@users.noreply.github.com>
2019-05-29 12:59:13 +0200

    League of legends rank display

--- bot/commands/getRank.js
+++ bot/commands/getRank.js
@@ -1,21 +1,41 @@
+var pascalCase = require('pascal-case')
+const Discord = require("discord.js");
+
 module.exports = {
     name: 'getrank',
     description: 'finds the rank of a person for you.',
     execute(message, args) {
         if (args.length < 1) {
-            throw 'Please provide an username.';
+            throw 'Provide an username.';
         }
         message.client.lolApi.get('euw1', 'summoner.getBySummonerName', args.join(" ")).then(data => {
             message.client.lolApi.get('euw1', 'league.getAllLeaguePositionsForSummoner', data.id).then(data => {
                 console.log(data);
+                var rank_data = []
                 for (var i = 0; i < data.length; i++) {
-                    var rank = data[i].queueType + " " + data[i].tier + " " + data[i].rank;
-                    if (rank.includes("BRONZE"))
-                        message.channel.send(rank + " :joy: :wheelchair:");
+                    var queueType = "";
+                    var winratio = data[i].wins / (data[i].wins + data[i].losses) * 100;
+                    switch(data[i].queueType){
+                        case "RANKED_FLEX_SR":
+                        queueType = "Ranked Flex 5v5"
+                        break;
+                        case "RANKED_SOLO_5x5":
+                        queueType = "Ranked Solo/Duo"
+                        break;
+                        case "RANKED_FLEX_TT":
+                        queueType = "Ranked Flex 3v3"
+                        break;
+                    }
+                    var rank = queueType + " " + pascalCase(data[i].tier) + " " + data[i].rank + " "+Math.floor(winratio)+"%";
+                    if (rank.includes("Bronze"))
+                       rank_data.push((rank) + " :joy: :wheelchair:");
                     else
-                        message.channel.send(rank);
+                       rank_data.push(rank);
+                
                 }
-
+                let botembed = new Discord.RichEmbed().setDescription(data[0].summonerName)
+                .addField("Ranks",rank_data)
+                message.channel.send(botembed);
             }).catch(function (error) {
                 message.channel.send("No user with the name " + args.join(" ") + " found.");
             });
@@ -23,4 +43,6 @@ module.exports = {
 
         });
     }
+    
+
 }
