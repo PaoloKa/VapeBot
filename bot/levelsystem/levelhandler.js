c228f851173ca0bf647e7b30180651dcb2035eee
PaoloKahn <30302837+PaoloKahn@users.noreply.github.com>
2019-08-06 23:21:51 +0200

    xp increase for each levelup

--- bot/levelsystem/levelhandler.js
+++ bot/levelsystem/levelhandler.js
@@ -3,13 +3,15 @@ const dl = require('discord-leveling');
 module.exports = {
 
     async addExp(message) {
-        var profile = await dl.Fetch(message.author.id)
+            var profile = await dl.Fetch(message.author.id)
             dl.AddXp(message.author.id, 10)
-            if (profile.xp + 10 > 10) {
+            var expNeeded = profile.level * 10;
+            if (profile.xp + 10 > expNeeded) {
                 await dl.AddLevel(message.author.id, 1)
                 await dl.SetXp(message.author.id, 0)
                 message.reply(`You just leveled up!! You are now level: ${profile.level + 1}`)
             }
     },
 
-}
+};
+
